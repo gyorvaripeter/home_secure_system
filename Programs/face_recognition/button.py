5b0228f36e192414f64582aaa301134e8507c3a3
gyorvaripeter <gyorvaripeter1996@gmail.com>
2020-01-15 21:47:39 +0100

    ascii characters deleted

--- Programs/face_recognition/button.py
+++ Programs/face_recognition/button.py
@@ -2,15 +2,15 @@ import RPi.GPIO as GPIO
 import time
 import os
 
-#21-es GPIO port beállítva nyomógombos bevitelre(high jel küldése) 
+#21es GPIO port beallitva nyomogombos bevitelre(high jel kuldese) 
 GPIO.setmode(GPIO.BCM) 
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(21)
     if input_state == False:
         print('Button Pressed')
-        #terminal parancs indítása a gomb megnyomására (tartalmazó mappába
-        #lépés .bashrc fájl átírásával került megoldásra
+        #terminal parancs inditasa a gomb megnyomasara (tartalmazo mappaba
+        #lepes .bashrc fajl atirasaval kerult megoldasra
         command = "python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle"
         os.system(command)
         time.sleep(5)
